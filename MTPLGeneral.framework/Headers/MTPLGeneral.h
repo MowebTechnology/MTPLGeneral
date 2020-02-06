f22d631611abd99619d9dd1bb6eab006b30c2d04
iOS Moweb <iosmoweb@moweb.com>
2020-02-06 15:42:46 +0530

    0.0.2 ready

---
+++ MTPLGeneral.framework/Headers/MTPLGeneral.h
@@ -0,0 +1,19 @@
+//
+//  MTPLGeneral.h
+//  MTPLGeneral
+//
+//  Created by Ankit Patel on 06/02/20.
+//  Copyright © 2020 Moweb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MTPLGeneral.
+FOUNDATION_EXPORT double MTPLGeneralVersionNumber;
+
+//! Project version string for MTPLGeneral.
+FOUNDATION_EXPORT const unsigned char MTPLGeneralVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MTPLGeneral/PublicHeader.h>
+
+
